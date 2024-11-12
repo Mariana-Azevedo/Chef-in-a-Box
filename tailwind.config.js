668b571acbd26f68a60114dfa8cbc1ed4ee2841c
Mariana-Azevedo <mariana.azevedo@ufrrj.br>
2024-11-11 23:56:42 -0300

    feat: componentizado layout e botão, criado tela e rota de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/**/*.edge",
+    "./resources/**/*.{js,ts,jsx,tsx,vue}",
+  ],
   theme: {
     extend: {},
   },
