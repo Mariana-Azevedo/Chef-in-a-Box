a64ae83c267089b6319d5ec104058ab7e24d5232
BrenosDias <brenosdias@ufrrj.br>
2024-11-15 23:30:52 -0300

    Validation

---
+++ app/validators/ingredient.ts
@@ -0,0 +1,13 @@
+import vine from '@vinejs/vine'
+
+export const createIngredientValidator = vine.compile(
+    vine.object({
+        name: vine.string().trim(),
+        description: vine.string().trim(),
+        unit: vine.string().trim(),
+        price: vine.number(),
+        image: vine.string().trim(),
+        imageType: vine.string().trim(),
+        quantity: vine.number(),
+    })
+  )
