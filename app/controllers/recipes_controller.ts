5ce8060cc0323ff2edff6eaf20199e29e6922001
Mariana-Azevedo <mariana.azevedo@ufrrj.br>
2024-12-16 21:53:51 -0300

    Mudado a logica de negocio da tabela e adicionado features no footer

--- app/controllers/recipes_controller.ts
+++ app/controllers/recipes_controller.ts
@@ -71,7 +71,6 @@ export default class RecipesController{
           unit: ingredient.unit,
           price: ingredient.price,
           quantity: ingredient.$extras.pivot_quantity,
-          unit: ingredient.unit,
         })),
       };
   
